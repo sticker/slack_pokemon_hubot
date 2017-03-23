f14b140287fbe750718784d956fb947745154293
sticker <hoshino.tetsuya@gmail.com>
2017-03-23 14:03:42 +0900

    modify item.img path

--- init/mongo_pokemonItem.js
+++ init/mongo_pokemonItem.js
@@ -1,2 +1,2 @@
 db.brain.remove( { key: "pokemonItem"} );
-db.brain.insert( { key: "pokemonItem", type: "_private", value: [ { "id" : 1, "name" : "スーパーボール", "price" : 4, "img" : "https://raw.githubusercontent.com/sticker/slack_pokemon/master/images/super_ball.png", "note" : "モンスターボールより少し性能がいい" }]} );
+db.brain.insert( { key: "pokemonItem", type: "_private", value: [ { "id" : 1, "name" : "スーパーボール", "price" : 4, "img" : "https://raw.githubusercontent.com/sticker/slack_pokemon/master/images/items/super_ball.png", "note" : "モンスターボールより少し性能がいい" }]} );
